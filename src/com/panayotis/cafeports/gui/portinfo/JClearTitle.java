8f4ae0caddb863bc806121612938641bfc6c766a
Panayotis Katsaloulis <Panayotis Katsaloulis@localhost>
2010-01-15 17:33:30 +0200

    info round corner is prettier

--- src/com/panayotis/cafeports/gui/portinfo/JClearTitle.java
+++ src/com/panayotis/cafeports/gui/portinfo/JClearTitle.java
@@ -28,19 +28,25 @@ public class JClearTitle extends JClearPanel {
         closeB = new JClearButton("/icons/buttons/close.png");
         JClearPanel left = new JClearPanel();
         JClearPanel right = new JClearPanel();
+        JClearPanel leftedge = new JClearPanel();
+        JClearPanel rightedge = new JClearPanel();
 
-        Dimension size = new Dimension(10, 1);
+        Dimension size = new Dimension(8, 8);
         tl.setPreferredSize(size);
         tr.setPreferredSize(size);
 
         closeB.setToolTipText("Close info window");
 
         left.setLayout(new BorderLayout());
-        left.add(tl, BorderLayout.WEST);
+        leftedge.setLayout(new BorderLayout());
+        leftedge.add(tl, BorderLayout.NORTH);
+        left.add(leftedge, BorderLayout.WEST);
         left.add(closeB, BorderLayout.CENTER);
 
         right.setLayout(new BorderLayout());
-        right.add(tr, BorderLayout.EAST);
+        rightedge.setLayout(new BorderLayout());
+        rightedge.add(tr, BorderLayout.NORTH);
+        right.add(rightedge, BorderLayout.EAST);
 
         label.setFont(label.getFont().deriveFont(label.getFont().getStyle() | java.awt.Font.BOLD));
         label.setHorizontalAlignment(JLabel.CENTER);
